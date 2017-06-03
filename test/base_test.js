0db450cabc9339712c5abd89afaf92e9d5829518
caizhenxing <caizhenxing@gmail.com>
2017-06-03 09:28:00 +0800

    发布

--- test/base_test.js
+++ test/base_test.js
@@ -1,9 +1,9 @@
 import '../src/base';
 
-describe( 'API', function () {
-  it( '混入', async function () {
-    const Class1 = class extends saasplat.mixins( saasplat.base ) {}
+describe('API', function() {
+  it('混入', async function() {
+    const Class1 = class extends saasplat.mixins(saasplat.base) {}
     const c1 = new Class1;
-  } )
+  })
 
-} )
+})
