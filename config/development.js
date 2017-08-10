a1b1ca8415b3b9209e3bc3b9d2f1f6ea18435c78
caizx <caizx@chanjet.com>
2017-08-10 19:32:52 +0800

    微调

--- config/development.js
+++ config/development.js
@@ -2,9 +2,9 @@ var app = require('../lib/app').default;
 var path = require('path');
 var fs = require('fs');
 
-var configfile = path.normalize(path.join(process.cwd(), 'test.json'));
+var configfile = path.normalize(path.join(process.cwd(), 'development.json'));
 if (!fs.existsSync(configfile)) {
-  console.warn('无法加载config，必须提供一个配置文件', configfile);
+  console.warn('无法加载开发配置信息，必须提供一个配置文件', configfile);
   console.log('更多信息', 'https://github.com/saas-plat/saas-plat-server');
 } else {
   var config = JSON.parse(fs.readFileSync(configfile));
@@ -22,7 +22,7 @@ if (!fs.existsSync(configfile)) {
     debug: true,
     logLevel: 'INFO'
   }, config));
-  instance.compile({log: true});
+  instance.compile({ log: true });
   instance.run().catch(function(err) {
     console.error(err);
   });
