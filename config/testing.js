6302003509c9587e3b26a8fd5d245fc63dc66509
caizx <caizx@chanjet.com>
2017-08-17 19:11:13 +0800

    调整测试

--- config/testing.js
+++ config/testing.js
@@ -1,4 +1,56 @@
-require('babel-register');
 require('babel-polyfill');
+var App = require('../lib/app').default;
 
-module.exports = require('./testing_es6');
+module.exports = function(args) {
+  var database = args.database || 'testdb',
+    username = args.username || 'root',
+    password = args.password || '',
+    db = args.db,
+    cfg = args.cfg;
+
+  // 测试db全部配置成mysql
+  var instance = new App(Object.assign({
+    appPath: process.cwd(),
+    modules: '*',
+    codePath: 'src',
+    eventBus: 'direct',
+    // port: 9900,
+    eventStoreage: {
+      storage: 'mysql_domain_event',
+      mysql: Object.assign({
+        username: username,
+        password: password,
+        database: database,
+        host: 'localhost',
+        dialect: 'mysql'
+      }, db)
+    },
+    snapshotStoreage: {
+      storage: null // 禁用快照
+    },
+    datadb: Object.assign({
+      username: username,
+      password: password,
+      database: database,
+      host: 'localhost',
+      dialect: 'mysql'
+    }, db),
+    systemdb: Object.assign({
+      username: username,
+      password: password,
+      database: database,
+      host: 'localhost',
+      dialect: 'mysql'
+    }, db),
+    querydb: Object.assign({
+      username: username,
+      password: password,
+      database: database,
+      host: 'localhost',
+      dialect: 'mysql'
+    }, db),
+    logLevel: 'All',
+    // ,debugOutput: true
+  }, cfg));
+  return instance.test();
+}
